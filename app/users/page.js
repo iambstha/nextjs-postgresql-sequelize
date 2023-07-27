7d4a036e9e2303c45ff48ed04dab97180e362fa8
unknown <iambstha@gmail.com>
2023-07-27 17:51:23 +0545

    update and delete user left

--- app/users/page.js
+++ app/users/page.js
@@ -24,14 +24,29 @@ const page = () => {
   }, []);
 
   return (
-    <div className=' flex flex-col justify-center items-center w-full '>
-      <h1 className=' p-4 '>All Users</h1>
-      <ul className=' flex gap-4 flex-wrap '>
-        {users && users.map(user => (
-          <li key={user.id}>{user.fname} {user.lname}</li>
-        ))}
-      </ul>
-    </div>
+    // <div className=' flex flex-col justify-center items-center w-full '>
+    //   <h1 className=' p-4 '>All Users</h1>
+    //   <ul className=' flex gap-4 flex-wrap '>
+    //     {users && users.map(user => (
+    //       <li key={user.id}>{user.fname} {user.lname}</li>
+    //     ))}
+    //   </ul>
+    // </div>
+
+<div>
+<h1>All Users</h1>
+<ul>
+    {users.map(user => (
+        <li key={user.id} className=' flex justify-center items-center gap-4 p-4 '>
+            <span className=' m-2 w-1/3 '>{user.fname}</span>
+            <span className=' m-2 w-1/3 '>{user.lname}</span>
+            <button onClick={() => handleUpdateClick(user.id)}  className=' border w-1/3 p-2 px-4 bg-blue-500 border-blue-500 text-white rounded '>Update</button>
+            <button onClick={() => handleDeleteClick(user.id)} className=' border w-1/3 p-2 px-4 bg-red-500 border-red-500 text-white rounded '>Delete</button>
+        </li>
+    ))}
+
+</ul>
+</div>
   );
 };
 
