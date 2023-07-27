fc43193d9ebad140e9555e9908b9fdceaf92e449
unknown <iambstha@gmail.com>
2023-07-27 16:51:13 +0545

    added console for logging

--- db/op/createItem.js
+++ db/op/createItem.js
@@ -4,6 +4,7 @@ import Item from "@/models/Item"
 const createItem = async (name,price) => {
     try {
         await Item.create({name,price})
+        console.log("Item succesfully created.")
     } catch (error) {
         console.log("Failed creating item.")
         throw Error
